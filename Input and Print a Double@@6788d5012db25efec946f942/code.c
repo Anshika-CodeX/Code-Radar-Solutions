569671f3ae0ce9a9b43871e6295810f092d86dd6
Anshika-CodeX <anshika0265.becse24@chitkara.edu.in>
2025-02-08 13:26:40 +0530

    Solution to Input and Print a Double

---
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// char* welcome() {
+//     return "Welcome to Code Radar!";
+// }
+
+int main() {
+    Double a;
+    scanf("lf",&a);
+    printf("You entered : %lf",a);
+    // printf("%s", welcome());
+    return 0;
+}
