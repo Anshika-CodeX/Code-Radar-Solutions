e5efacffb7ad99c152ecc7c7839b5e429e6c0b4d
Anshika-CodeX <anshika0265.becse24@chitkara.edu.in>
2025-02-09 17:29:04 +0530

    Solution to Check Leap Year

--- Check Leap Year@@6788dd63490637cfc44e1b90/code.c
+++ Check Leap Year@@6788dd63490637cfc44e1b90/code.c
@@ -9,14 +9,10 @@ int main() {
     scanf("%d",&year);
     if ((year%4==0 && year%100!=0) || (year%400==0))
     {
-
-    
     printf("Leap year");
     }
     else{
         printf("Not a Leap year");
     }
-
-    // printf("%s", welcome());
     return 0;
 }
